2bbf30f4125219a540887788211e60f331807cdf
thisisscatterbrain <arno.luebke@googlemail.com>
2013-11-27 10:43:30 +0900

    Implemented ellipsoidal camera movement

--- ToudaijiCube/ToudaijiCube/ControllerDelegate.m
+++ ToudaijiCube/ToudaijiCube/ControllerDelegate.m
@@ -44,7 +44,7 @@
 {
     self.skyBoxRenderer = [[SkyBoxRenderer alloc] initWithCubeMap:filename];
     [self.skyBoxRenderer setScale:5.0f];
-    [self.skyBoxRenderer setBottomAlphaMask:@"amap3.png"];
+    [self.skyBoxRenderer setBottomAlphaMask:@"amap.png"];
 }
 
 - (void)dealloc
@@ -112,13 +112,25 @@
 
 //    NSLog(@"yaw = %f", GLKMathRadiansToDegrees(self.motionManager.deviceMotion.attitude.yaw));
 //    NSLog(@"roll = %f", GLKMathRadiansToDegrees(self.motionManager.deviceMotion.attitude.roll + GLKMathDegreesToRadians(90.0)));
-//    
-    GLKVector3 v = GLKVector3Make(0.0f, 0.0f, -2.0f);
+//
+
+    // rotate camera on sphere
+    GLKVector3 v = GLKVector3Make(0.0f, 0.0f, -1.0f); // original camera position
     GLKMatrix4 ry = GLKMatrix4MakeRotation(self.motionManager.deviceMotion.attitude.yaw, 0, 1, 0);
     GLKMatrix4 rx = GLKMatrix4MakeRotation(-self.motionManager.deviceMotion.attitude.roll - GLKMathDegreesToRadians(90.0), 1, 0, 0);
     
     v = GLKMatrix4MultiplyVector3(ry, v);
     v = GLKMatrix4MultiplyVector3(rx, v);
+    
+    // set camera from sphere to the elipsoid (defined by a, b and c) by
+    // shooting a ray from the origin through the camera position on the sphere
+    // and calculating its intersection with the sphere.
+    float a = 2.0f, b = 2.0f, c = 4.0f;
+    float k = 1.0f/sqrtf((v.x*v.x)/(a*a) + (v.y*v.y)/(b*b) + (v.z*v.z)/(c*c));
+    
+    v = GLKVector3MultiplyScalar(v, k);
+    
+    // translate skybox and .obj in opposite direction of the camera postion
     v.x *= -1.0f;
     v.y *= -1.0f;
     v.z *= -1.0f;
@@ -131,7 +143,8 @@
     
     
     
-    // set the rotation of the sky box and the .obj.
+    // set the rotation of the sky box and the .obj to opposite rotation of the
+    // camera rotation
     [self.skyBoxRenderer setRotationZ:self.motionManager.deviceMotion.attitude.pitch];
     [self.skyBoxRenderer setRotationY:-self.motionManager.deviceMotion.attitude.yaw];
     [self.skyBoxRenderer setRotationX:self.motionManager.deviceMotion.attitude.roll + GLKMathDegreesToRadians(90.0)];
